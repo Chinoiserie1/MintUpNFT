9c28cf2166f69f4bee2bea8e919b1ad5a0148604
chixx <lucottejeremie@gmail.com>
2023-05-13 17:35:02 +0200

    add contract MintUpFactory

--- src/MintUpFactory.sol
+++ src/MintUpFactory.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract MintUpFactory {
-  
+import { MintUpNft } from "./MintUpNft.sol";
+import { Initialisaser } from "./Interfaces/IMintUpNft.sol";
+
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+
+contract MintUpFactory is Ownable {
+  MintUpNft[] public deployedCollection;
+
+  event NewCollectionDeployed(address indexed collectionAddress);
+
+  function deployNewCollection(Initialisaser calldata initParams) external onlyOwner {
+    MintUpNft newCollection = new MintUpNft(initParams);
+    deployedCollection.push(newCollection);
+    emit NewCollectionDeployed(address(newCollection));
+  }
 }
